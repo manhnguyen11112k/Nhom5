705848e43d8b76d95e770f626a90e324623eb83c
manhnguyen11112k <manhnguyen11112k@gmail.com>
2021-11-28 23:10:34 +0700

    th

--- SHOP1/shop.sql
+++ SHOP1/shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 21, 2021 lúc 05:10 PM
+-- Thời gian đã tạo: Th10 28, 2021 lúc 05:07 PM
 -- Phiên bản máy phục vụ: 10.4.20-MariaDB
 -- Phiên bản PHP: 7.3.29
 
@@ -51,6 +51,24 @@ INSERT INTO `dangnhap` (`id`, `level`, `taikhoan`, `matkhau`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `giohang`
+--
+
+CREATE TABLE `giohang` (
+  `id` int(10) NOT NULL,
+  `masp` varchar(10) NOT NULL,
+  `tensp` varchar(30) NOT NULL,
+  `chitiet` varchar(1000) NOT NULL,
+  `gia` int(11) NOT NULL,
+  `loaisp` varchar(10) NOT NULL,
+  `anh` varchar(100) NOT NULL,
+  `soluong` int(10) NOT NULL,
+  `thanhtien` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `sanpham`
 --
 
@@ -75,7 +93,26 @@ INSERT INTO `sanpham` (`masp`, `tensp`, `nxb`, `xuatxu`, `baohanh`, `tinhtrang`,
 ('1', 'Laptop MSI Modern 14 B5M 064VN', 'MSI', 'Chính hãng', '2 năm', 'Mới 100%', 'MSI Modern ', 16990000, 'laptop', '../../../image/tt5_lt1.jpg'),
 ('2', 'MacBook Pro 13 2020 M1 16GB 25', 'APPLE', 'Chính hãng', '12 tháng', 'Mới 100%', 'Bộ vi xử lý Apple M1 giúp MacBook Pro tốc độ và mạnh mẽ hơn bao giờ hết.', 37990000, 'laptop', '../../../image/lt2.jpg'),
 ('3', 'Asus3', 'Asus', 'Anh', '1 năm', 'mới', 'rất mượt', 35000000, 'laptop', '../../../image/laptop1.jpg'),
-('4', 'Asus4', 'Asus', 'Anh', '1 năm', 'mới', 'mới vl', 35000000, 'laptop', '../../../image/laptop1.jpg');
+('4', 'Asus4', 'Asus', 'Anh', '1 năm', 'mới', 'mới vl', 35000000, 'linhkien', '../../../image/laptop1.jpg'),
+('5', 'Laptop MSI Modern 14 B5M 064VN', 'Asus', 'Anh', '1 năm', 'mới', 'rất mượt', 35000000, 'laptop', '../../../image/lt2.jpg'),
+('6', 'Asus6', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'rất mượt', 35000000, 'laptop', '../../../image/laptop1.jpg'),
+('7', 'Asus7', 'Asus', 'Anh', '1 năm', 'mới', 'rất mượt', 35000000, 'laptop', '../../../image/tt2_lt1.jpg'),
+('8', 'Asus8', 'Asus', 'Anh', '1 năm', 'mới', 'rất mượt', 35000000, 'laptop', '../../../image/tt3_lt1.png'),
+('9', 'Asus9', 'Asus', 'Anh', '1 năm', 'mới', 'rất mượt', 35000000, 'laptop', '../../../image/tt4_lt1.jpg'),
+('10', 'Asus10', 'Asus', 'Anh', '1 năm', 'mới', 'rất mượt', 35000000, 'laptop', '../../../image/laptop1.jpg'),
+('11', 'Asus11', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'rất mượt', 16990000, 'laptop', '../../../image/lt2.jpg'),
+('12', 'Asus12', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'rất mượt', 16990000, 'linhkien', '../../../image/laptop1.jpg'),
+('13', 'Asus13', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'rất mượt', 35000000, 'laptop', '../../../image/laptop1.jpg'),
+('14', 'Asus14', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'rất mượt', 35000000, 'linhkien', '../../../image/laptop1.jpg'),
+('15', 'Asus15', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'rất mượt', 35000000, 'laptop', '../../../image/laptop1.jpg'),
+('16', 'Asus16', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'MSI Modern 14 sở hữu bộ vi xử lý AMD 5000 - Thế hệ mới nhất hiện nay. Khi mà kết hợp với card tích hợp AMD Radeon mang lại hiệu năng cực kì mạnh mẽ để làm đồ họa, edit/render video và bạn có thể chơi mượt các tựa game online hiện nay trên chiếc laptop văn phòng này.     Với RAM 8GB có sẵn, bạn có thể vừa làm việc trên nhiều tabs cùng lúc mà không lo sợ giật lag. Nếu nhu cầu công việc của bạn cao hơn, cần làm nhiều việc hơn, bạn cũng có thể nâng cấp RAM lên thành 16GB.    Ổ cứng SSD NVMe cho tốc độ xử lý và khởi động máy cực nhanh với thời gian chỉ trong vài giây. Có thể nói đây là một chiếc laptop văn phòng cực kỳ đẳng cấp và đa năng, khi mà bạn có thể sử dụng các ứng dụng liên quan đến đồ họa trên Adobe hoặc 3D chuyên nghiệp và chơi game một cách vô cùng mượt mà, trơn tru.     Hệ thống tản nhiệt tối ưu  Máy được trang bị hệ thống làm mát Exclusive Cooler Boost với 2 quạt tản lớn chuyên dụng và 3 ống dẫn nhiệt. Việc trang bị này sẽ đảm bảo cho máy luôn có nhiệt độ ổn định và mát mẻ khi người dùng đang render ', 35000000, 'laptop', '../../../image/laptop1.jpg'),
+('17', 'Asus17', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'Bộ vi xử lý Apple M1 giúp MacBook Pro tốc độ và mạnh mẽ hơn bao giờ hết.', 35000000, 'laptop', '../../../image/laptop1.jpg'),
+('18', 'Asus1', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'rất mượt', 35000000, 'linhkien', '../../../image/laptop1.jpg'),
+('17', 'Asus17', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'rất mượt', 35000000, 'linhkien', '../../../image/laptop1.jpg'),
+('20', 'Asus20', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'MSI Modern 14 sở hữu bộ vi xử lý AMD 5000 - Thế hệ mới nhất hiện nay. Khi mà kết hợp với card tích hợp AMD Radeon mang lại hiệu năng cực kì mạnh mẽ để làm đồ họa, edit/render video và bạn có thể chơi mượt các tựa game online hiện nay trên chiếc laptop văn phòng này.     Với RAM 8GB có sẵn, bạn có thể vừa làm việc trên nhiều tabs cùng lúc mà không lo sợ giật lag. Nếu nhu cầu công việc của bạn cao hơn, cần làm nhiều việc hơn, bạn cũng có thể nâng cấp RAM lên thành 16GB.    Ổ cứng SSD NVMe cho tốc độ xử lý và khởi động máy cực nhanh với thời gian chỉ trong vài giây. Có thể nói đây là một chiếc laptop văn phòng cực kỳ đẳng cấp và đa năng, khi mà bạn có thể sử dụng các ứng dụng liên quan đến đồ họa trên Adobe hoặc 3D chuyên nghiệp và chơi game một cách vô cùng mượt mà, trơn tru.     Hệ thống tản nhiệt tối ưu  Máy được trang bị hệ thống làm mát Exclusive Cooler Boost với 2 quạt tản lớn chuyên dụng và 3 ống dẫn nhiệt. Việc trang bị này sẽ đảm bảo cho máy luôn có nhiệt độ ổn định và mát mẻ khi người dùng đang render ', 35000000, 'laptop', '../../../image/laptop1.jpg'),
+('21', 'Asus21', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'rất mượt', 35000000, 'laptop', '../../../image/laptop1.jpg'),
+('22', 'Asus22', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'rất mượt', 35000000, 'laptop', '../../../image/laptop1.jpg'),
+('24', 'Asus24', 'Asus', 'Anh', '1 năm', 'Mới 100%', 'rất mượt', 35000000, 'linhkien', '../../../image/laptop1.jpg');
 
 -- --------------------------------------------------------
 
